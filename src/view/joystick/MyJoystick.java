bdfcc8b0e9b464cec0c54457bb8787482d51bbb3
DanBN <dbn290895@gmail.com>
2021-06-13 21:32:29 +0300

    updated commit 13.06

--- src/view/joystick/MyJoystick.java
+++ src/view/joystick/MyJoystick.java
@@ -8,22 +8,31 @@ import javafx.scene.layout.AnchorPane;
 
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class MyJoystick extends AnchorPane {
 
     public DoubleProperty aileron, elevator, rudder, throttle;
     public MyJoystickController myJoystickController;
+    public HashMap<String,DoubleProperty> joystickMap;
     public MyJoystick(){
         super();
         try{
             FXMLLoader fxl = new FXMLLoader();
             AnchorPane joy = (AnchorPane) fxl.load(getClass().getResource("MyJoystick.fxml").openStream());
             myJoystickController=fxl.getController();
+            joystickMap = new HashMap<>();
 
-            aileron=myJoystickController.aileron;
-            elevator=myJoystickController.elevator;
-            rudder=myJoystickController.rudder.valueProperty();
-            throttle=myJoystickController.throttle.valueProperty();
+            joystickMap.put("aileron", myJoystickController.aileron);
+            joystickMap.put("elevator", myJoystickController.elevator);
+            joystickMap.put("rudder", myJoystickController.rudder.valueProperty());
+            joystickMap.put("throttle", myJoystickController.throttle.valueProperty());
+
+
+//            aileron=myJoystickController.aileron;
+//            elevator=myJoystickController.elevator;
+//            rudder=myJoystickController.rudder.valueProperty();
+//            throttle=myJoystickController.throttle.valueProperty();
             myJoystickController.paint();
 
             this.getChildren().add(joy);
